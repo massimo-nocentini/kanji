7e71d4259d4f731978e3e065c4f59d0152c51f48
Massimo Nocentini <massimo.nocentini@gmail.com>
2018-01-05 10:51:40 +0100

    Refine and use `seed` function.

--- randletters.py
+++ randletters.py
@@ -1,12 +1,15 @@
 
-from random import randrange
+from random import randrange, seed
 
-cols = 41
+seed(1024)
+
+cols = 40
 
 def row():
     chars = [chr(ord('a') + randrange(26)) for i in range(cols)]
+    #chars = map(lambda c: r'\small{{{}}}'.format(c), chars)
     return ' & '.join(chars) + r' \\' + '\n'
-    
+
 with open('giulia-tabular.tex', 'w') as f:
     f.write(r'\begin{tabular}' + '{{{}}}'.format('c' * cols) + '\n')
     for i in range(31):
